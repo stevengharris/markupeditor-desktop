f3bf1d27a447820d3b4283477082e0d88d1f7cfe
Steven G. Harris <steven.g.harris@gmail.com>
2025-08-04 14:18:35 -0700

    Track openFilePath, disable save if not defined

--- main.js
+++ main.js
@@ -11,12 +11,15 @@ const createWindow = () => {
     win.loadFile('index.html')
 }
 
+let openFilePath = null;
+
 app.whenReady().then(() => {
     createWindow()
     const template = (process.platform === 'darwin') ? macTemplate : nonMacTemplate;
     if (process.env.NODE_ENV !== 'production') template.push(debugMenu);
     const menu = Menu.buildFromTemplate(template)
     Menu.setApplicationMenu(menu)
+    setOpenFilePath(null)
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
@@ -30,6 +33,17 @@ app.on('window-all-closed', () => {
     }
 })
 
+function getWebContents() {
+    return BrowserWindow.getFocusedWindow()?.webContents
+}
+
+/** Track the openFilePath, which may be null */
+function setOpenFilePath(string) {
+    openFilePath = string
+    const saveItem = Menu.getApplicationMenu().getMenuItemById('save');
+    saveItem.enabled = string != null
+}
+
 /** For now, use the markupeditor-base web site for learning more about the desktop MarkupEditor */
 async function learnMore() {
     const { shell } = require('electron')
@@ -41,55 +55,71 @@ async function learnMore() {
  * Note when setting the HTML, base is set based on the directory of the file.
  */
 async function openDocument() {
-    const { cancelled, filePaths } = await dialog.showOpenDialog({
+    const { canceled, filePaths } = await dialog.showOpenDialog({
         properties: ['openFile'],
         filters: [
             { name: 'HTML', extensions: ['html', 'htm'] }
         ]
     });
 
-    if (!cancelled) {
+    if (!canceled) {
         let filePath = filePaths[0]
         fs.readFile(filePath, 'utf8', (err, text) => {
             if (err) {
                 console.error('Error reading file:', err);
                 return;
             }
-            let webContents = BrowserWindow.getFocusedWindow()?.webContents;
-            if (webContents) {
-                // Get rid of newlines and escape single quotes
-                let escapedText = text.replace(/(\r\n|\n|\r)/g, "").replaceAll("'", "&#039;");
-                let base = path.dirname(filePath) + '/'     // Don't forget the trailing slash!
-                let setHTMLCommand = `MU.setHTML('${escapedText}', true, '${base}')`
-                webContents.executeJavaScript(setHTMLCommand)
-                    .catch((error) => {
-                        console.error('Error setting contents:', error);
-                    });
-            }
+            // Get rid of newlines and escape single quotes
+            let escapedText = text.replace(/(\r\n|\n|\r)/g, "").replaceAll("'", "&#039;");
+            let base = path.dirname(filePath) + '/'     // Don't forget the trailing slash!
+            let setHTMLCommand = `MU.setHTML('${escapedText}', true, '${base}')`
+            getWebContents()?.executeJavaScript(setHTMLCommand)
+                .then(() => {setOpenFilePath(filePath)})
+                .catch((error) => {
+                    console.error('Error setting contents:', error);
+                });
         });
     }
 }
 
 async function newDocument() {
-    let webContents = BrowserWindow.getFocusedWindow()?.webContents;
-    webContents?.executeJavaScript('MU.emptyDocument()')
+    getWebContents()?.executeJavaScript('MU.emptyDocument()')
+        .then(() => {setOpenFilePath(null)})
         .catch((error) => {
             console.error('Error creating empty document:', error);
         });
 }
 
-/** Save HTML contents to a file */
 async function saveDocument() {
-    const { cancelled, filePath } = await dialog.showSaveDialog({
+    if (!openFilePath) return
+    getWebContents()?.executeJavaScript('MU.getHTML()')
+        .then((html) => {
+            fs.writeFile(openFilePath, html, 'utf8', (err) => {
+                if (err) {
+                    console.error('Error writing file:', err);
+                    return;
+                }
+            })
+        })
+        .catch((error) => {
+            console.error('Error getting contents:', error);
+        }
+    );
+}
+
+/** Save HTML contents to a file */
+async function saveDocumentAs() {
+    const options = {
         properties: ['createDirectory', 'showOverwriteConfirmation'],
         filters: [
             { name: 'HTML', extensions: ['html', 'htm'] }
         ]
-    });
+    }
+    if (openFilePath) options.defaultPath = openFilePath
+    const { canceled, filePath } = await dialog.showSaveDialog(options);
 
-    if (!cancelled) {
-        let webContents = BrowserWindow.getFocusedWindow()?.webContents;
-        webContents?.executeJavaScript('MU.getHTML()')
+    if (!canceled) {
+        getWebContents()?.executeJavaScript('MU.getHTML()')
             .then((html) => {
                 fs.writeFile(filePath, html, 'utf8', (err) => {
                     if (err) {
@@ -98,6 +128,7 @@ async function saveDocument() {
                     }
                 })
             })
+            .then(()=>{setOpenFilePath(filePath)})
             .catch((error) => {
                 console.error('Error getting contents:', error);
             });
@@ -150,7 +181,12 @@ const macTemplate = [
             {
                 label: 'Save',
                 accelerator: 'Cmd+S',
-                click: saveDocument
+                click: saveDocument,
+                id: 'save'
+            },
+            {
+                label: 'Save As...',
+                click: saveDocumentAs
             },
             { role: 'close' }
         ]
