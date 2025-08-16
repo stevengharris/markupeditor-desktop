4d984154145f5ebf3eeafd0415893ca08a821f59
Steven G. Harris <steven.g.harris@gmail.com>
2025-08-16 16:46:39 -0700

    Initial Format menu

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, dialog, nativeImage, ipcMain, BrowserWindow, Menu } = require('electron')
+const { insertLinkCommand, insertImageCommand, toggleBold, toggleItalic, toggleCode } = require('markupeditor-base')
 const fs = require('node:fs')
 const path = require('path')
 
@@ -19,15 +20,21 @@ const createWindow = () => {
         icon: appIcon, // Set the window icon
     })
     win.on('close', quitIfApproved)     // Need this *and* the app.on('before-quit')
+
+    // One-time-only, set the application menu up based on the markupEditorConfig.
+    // This way, the config can be set up explicitly in index.html or will get the 
+    // "standard" defaults for the MarkupEditor, but either way, the application 
+    // menu contents and keymap will match.
+    win.once('ready-to-show', async () => {
+        let config = await getWebContents()?.executeJavaScript('MU.markupEditorConfig')
+        setApplicationMenu(config)
+        win.show()
+    })
     win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
     createWindow()
-    const template = (process.platform === 'darwin') ? macTemplate : nonMacTemplate;
-    if (process.env.NODE_ENV !== 'production') template.push(debugMenu);
-    const menu = Menu.buildFromTemplate(template)
-    Menu.setApplicationMenu(menu)
     setOpenFilePath(null)
 
     // Respond to messages sent from from the MarkupDelegate in setup.js
@@ -296,122 +303,297 @@ async function saveDocumentAs() {
     }
 }
 
-/** For non-production use, return a Debug menu that can be appended to the standard template */
-const debugMenu = {
-    label: 'Debug',
-    submenu: [
-        {
-            label: 'Open dev tools',
-            click: () => { BrowserWindow.getFocusedWindow().webContents.openDevTools() } 
-        }
-    ]
+/** Menu-related */
+
+function setApplicationMenu(config) {
+    const template = (process.platform === 'darwin') ? macTemplate(config) : nonMacTemplate;
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 }
 
 /** Return a template suitable for `Menu.buildFromTemplate` on a Mac */
-const macTemplate = [
-    { label: '' },  // Somewhat mindboggling, but see https://stackoverflow.com/a/55262353/8968411
-    // { role: 'appMenu' }
-    {
-        label: app.name,
-        submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'services' },
-            { type: 'separator' },
-            { role: 'hide' },
-            { role: 'hideOthers' },
-            { role: 'unhide' },
-            { type: 'separator' },
-            { role: 'quit' }
-        ]
-    },
-    // { role: 'fileMenu' }
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Open',
-                accelerator: 'Cmd+N',
-                click: openDocument
-            },
-            {
-                label: 'New',
-                accelerator: 'Cmd+O',
-                click: newDocument
-            },
-            {
-                label: 'Save',
-                accelerator: 'Cmd+S',
-                click: saveDocument,
-                id: 'save'
-            },
-            {
-                label: 'Save As...',
-                click: saveDocumentAs
-            },
-            { role: 'close' }
-        ]
-    },
-    // { role: 'editMenu' }
-    {
-        label: 'Edit',
-        submenu: [
-            { role: 'undo' },
-            { role: 'redo' },
-            { type: 'separator' },
-            { role: 'cut' },
-            { role: 'copy' },
-            { role: 'paste' },
-            { role: 'pasteAndMatchStyle' },
-            { role: 'delete' },
-            { role: 'selectAll' },
-            { type: 'separator' },
-            {
-                label: 'Speech',
-                submenu: [
-                    { role: 'startSpeaking' },
-                    { role: 'stopSpeaking' }
-                ]
-            }
-        ]
-    },
-    // { role: 'viewMenu' }
-    {
-        label: 'View',
-        submenu: [
-            { role: 'reload' },
-            { role: 'forceReload' },
-            { role: 'toggleDevTools' },
-            { type: 'separator' },
-            { role: 'resetZoom' },
-            { role: 'zoomIn' },
-            { role: 'zoomOut' },
-            { type: 'separator' },
-            { role: 'togglefullscreen' }
-        ]
-    },
-    // { role: 'windowMenu' }
-    {
-        label: 'Window',
-        submenu: [
-            { role: 'minimize' },
-            { role: 'zoom' },
-            { type: 'separator' },
-            { role: 'front' },
-            { type: 'separator' },
-            { role: 'window' }
-        ]
-    },
-    {
-        role: 'help',
-        submenu: [
-            {
-                label: 'Learn More',
-                click: learnMore
-            }
-        ]
+function macTemplate(config) {
+    let keymap = config.keymap
+    let template = [
+        { label: '' },  // Somewhat mindboggling, but see https://stackoverflow.com/a/55262353/8968411
+        // { role: 'appMenu' }
+        {
+            label: app.name,
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideOthers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        },
+        // { role: 'fileMenu' }
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Open',
+                    accelerator: 'Cmd+N',
+                    click: openDocument
+                },
+                {
+                    label: 'New',
+                    accelerator: 'Cmd+O',
+                    click: newDocument
+                },
+                {
+                    label: 'Save',
+                    accelerator: 'Cmd+S',
+                    click: saveDocument,
+                    id: 'save'
+                },
+                {
+                    label: 'Save As...',
+                    click: saveDocumentAs
+                },
+                { role: 'close' }
+            ]
+        },
+        // { role: 'editMenu' }
+        {
+            label: 'Edit',
+            submenu: [
+                { 
+                    role: 'undo',
+                    accelerator: acceleratorFor(keymap.undo)
+                },
+                { 
+                    role: 'redo',
+                    accelerator: acceleratorFor(keymap.redo)
+                },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'pasteAndMatchStyle' },
+                { role: 'delete' },
+                { role: 'selectAll' },
+                { type: 'separator' },
+                {
+                    label: 'Speech',
+                    submenu: [
+                        { role: 'startSpeaking' },
+                        { role: 'stopSpeaking' }
+                    ]
+                }
+            ]
+        }
+    ]
+    template.push(formatMenu(config))
+    template.push(...[
+        // { role: 'viewMenu' }
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forceReload' },
+                { role: 'toggleDevTools' },
+                { type: 'separator' },
+                { role: 'resetZoom' },
+                { role: 'zoomIn' },
+                { role: 'zoomOut' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        // { role: 'windowMenu' }
+        {
+            label: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'zoom' },
+                { type: 'separator' },
+                { role: 'front' },
+                { type: 'separator' },
+                { role: 'window' }
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click: learnMore
+                }
+            ]
+        }
+    ])
+    return template
+}
+
+function formatMenu(config) {
+    let menu = {label: 'Format'}
+    let {link, image, table, bullet, number, indent, outdent, bold, italic, underline, strikethrough, code, subscript, superscript, search} = config.keymap
+    let submenu = []
+    if (link) {
+        submenu.push({
+            label: 'Insert Link',
+            accelerator: acceleratorFor(link),
+            click: insertLink
+        })
     }
-]
+    if (image) {
+        submenu.push({
+            label: 'Insert Image',
+            accelerator: acceleratorFor(image),
+            click: insertImage
+        })
+    }
+    if (table) {
+        submenu.push({
+            label: 'Insert Table',
+            accelerator: acceleratorFor(table),
+            click: insertTable
+        })
+    }
+    if (bullet) {
+        submenu.push({
+            label: 'Bullet List',
+            accelerator: acceleratorFor(bullet),
+            click: () => { getWebContents().executeJavaScript('MU.toggleListType("UL")') }
+        })
+    }
+    if (indent) {
+        submenu.push({
+            label: 'Indent',
+            accelerator: acceleratorFor(indent),
+            click: () => { getWebContents().executeJavaScript('MU.indent()') }
+        })
+    }
+    if (outdent) {
+        submenu.push({
+            label: 'Outdent',
+            accelerator: acceleratorFor(outdent),
+            click: () => { getWebContents().executeJavaScript('MU.outdent()') }
+        })
+    }
+    if (number) {
+        submenu.push({
+            label: 'Number List',
+            accelerator: acceleratorFor(number),
+            click: () => { getWebContents().executeJavaScript('MU.toggleListType("OL")') }
+        })
+    }
+    if (bold) {
+        submenu.push({
+            label: 'Bold',
+            accelerator: acceleratorFor(bold),
+            click: () => { getWebContents().executeJavaScript('MU.toggleBold()') }
+        })
+    }
+    if (italic) {
+        submenu.push({
+            label: 'Italic',
+            accelerator: acceleratorFor(italic),
+            click: () => { getWebContents().executeJavaScript('MU.toggleItalic()') }
+        })
+    }
+    if (underline) {
+        submenu.push({
+            label: 'Underline',
+            accelerator: acceleratorFor(underline),
+            click: () => { getWebContents().executeJavaScript('MU.toggleUnderline()') }
+        })
+    }
+    if (strikethrough) {
+        submenu.push({
+            label: 'Strikethrough',
+            accelerator: acceleratorFor(strikethrough),
+            click: () => { getWebContents().executeJavaScript('MU.toggleStrike()') }
+        })
+    }
+    if (code) {
+        submenu.push({
+            label: 'Code',
+            accelerator: acceleratorFor(code),
+            click: () => { getWebContents().executeJavaScript('MU.toggleCode()') }
+        })
+    }
+    if (subscript) {
+        submenu.push({
+            label: 'Subscript',
+            accelerator: acceleratorFor(subscript),
+            click: () => { getWebContents().executeJavaScript('MU.toggleSubscript()') }
+        })
+    }
+    if (superscript) {
+        submenu.push({
+            label: 'Superscript',
+            accelerator: acceleratorFor(superscript),
+            click: () => { getWebContents().executeJavaScript('MU.toggleSuperscript()') }
+        })
+    }
+    if (search) {
+        submenu.push({
+            label: 'Search',
+            accelerator: acceleratorFor(search),
+            click: () => { getWebContents().executeJavaScript('MU.toggleSearch()') }
+        })
+    }
+    menu.submenu = submenu
+    return menu
+}
+
+/**
+ * Return a string suitable to be used as a menu Accelerator in Electron.
+ * 
+ * ProseMirror keymaps strings consist of keys separated by "-", while Electron uses "+".
+ * ProseMirror uses "Meta" as a generic term for "Alt" or "Option".
+ * ProseMirror uses "Mod" as a generic term for "Command" or "Cmd".
+ * Electron Accelerators use uppercase only for letter keys
+ * 
+ * @param {string} keymap   A keymap string used by the MarkupEditor which is based on ProseMirror
+ * @returns {string}        An accelerator string for Electron (https://www.electronjs.org/docs/latest/api/accelerator)
+ */
+function acceleratorFor(keymap) {
+    let pm = (Array.isArray(keymap)) ? keymap[0] : keymap;
+    let keys = pm.split('-')
+    let accelerator = ''
+    // The keys up until the last one are modifiers
+    for (i = 0; i < keys.length - 1; i++) {
+        let key = keys[i]
+        switch (key) {
+            case 'Mod':
+                accelerator += 'Cmd'
+                break
+            case 'Meta':
+                accelerator += 'Alt'
+                break;
+            default:
+                accelerator += key
+        }
+        accelerator += '+'
+    }
+    let key = keys[keys.length - 1]
+    if (key.length == 1) {
+        accelerator += key.toUpperCase()
+    } else {
+        accelerator += key
+    }
+    return accelerator
+}
+
+function insertLink() {
+    console.log("Insert a link")
+}
+
+function insertImage() {
+    console.log("Insert an image")
+}
+
+function insertTable() {
+    console.log("Insert a table")
+}
+
 
 /** Return a template suitable for `Menu.buildFromTemplate` on a non-Mac device (haha, tested only on a Mac, though) */
 const nonMacTemplate = [
